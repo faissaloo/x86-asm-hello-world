275c5339bcd53068d12f92df63ba892bd24c84d7
faissaloo <faissaloo@gmail.com>
2019-02-23 20:37:25 +0000

    Better convention for string definition, fix bug with exit code

--- hello_world.asm
+++ hello_world.asm
@@ -1,20 +1,18 @@
-section	.data
-  message db `Hello world!\n`
-  message_length equ $ - message
-
-  stdout equ 1
-
-  sys_exit equ 1
-  sys_write equ 4
+; define_string label string
+; Defines the string and saves the length in a macro called <label>_length
+%macro define_string 2
+  %1 db %2
+  %strlen %1_length %2
+%endmacro
 
 ; print_clobber string, length, return_address
 ; Writes a string to stdout, clobbering registers in the process for efficiency
-%macro print_clobber 3
+%macro print_clobber 2
   mov eax, sys_write
   mov ebx, stdout
   mov ecx, %1
-  mov edx, %2
-  push %3
+  mov edx, %1_length
+  push %2
   lea ebp, [esp-12]
   sysenter
 %endmacro
@@ -22,15 +20,24 @@ section	.data
 ; exit
 %macro exit 0
   mov eax, sys_exit
+  xor ebx, ebx
   mov ebp, esp
   sysenter
 %endmacro
+
+section	.data
+  define_string message, `Hello world!\n`
+
+  stdout equ 1
+
+  sys_exit equ 1
+  sys_write equ 4
 
 section	.text
   global _start
 
 _start:
-  print_clobber message, message_length, _exit
+  print_clobber message, _exit
 
 _exit:
   exit
