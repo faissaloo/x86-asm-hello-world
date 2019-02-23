c5f013c2b50196ff3d3e5a10375ba0b2151aec25
faissaloo <faissaloo@gmail.com>
2019-02-23 21:06:12 +0000

    Add macros for register assigment

--- hello_world.asm
+++ hello_world.asm
@@ -1,3 +1,19 @@
+; zero register
+; Sets a register to zero, note that this will affect CFLAGS
+%macro zero 1
+  xor %1, %1
+%endmacro
+
+; set
+; Sets a register to a constant in the most efficient way possible
+%macro set 2
+  %if %2==0
+    zero %1
+  %else
+    mov %1, %2
+  %endif
+%endmacro
+
 ; define_string label string
 ; Defines the string and saves the length in a macro called <label>_length
 %macro define_string 2
@@ -8,10 +24,10 @@
 ; print_clobber string, length, return_address
 ; Writes a string to stdout, clobbering registers in the process for efficiency
 %macro print_clobber 2
-  mov eax, sys_write
-  mov ebx, stdout
-  mov ecx, %1
-  mov edx, %1_length
+  set eax, sys_write
+  set ebx, stdout
+  set ecx, %1
+  set edx, %1_length
   push %2
   lea ebp, [esp-12]
   sysenter
@@ -19,9 +35,9 @@
 
 ; exit
 %macro exit 0
-  mov eax, sys_exit
-  xor ebx, ebx
-  mov ebp, esp
+  set eax, sys_exit
+  set ebx, 0
+  set ebp, esp
   sysenter
 %endmacro
 
